94a406f68c0ca9d9a5012f203fc8cdaa889b52b8
BinnyJ <binnyjohn39@gmail.com>
2020-11-02 23:46:21 -0500

    First project one

---
+++ src/paymentsystem/billpayement.java
@@ -0,0 +1,8 @@
+package paymentsystem;
+
+public class billpayement {
+    public static void main(String[] args) {
+        System.out.println("Java is fun");
+        System.out.println("xxxxxyyyy");
+    }
+}
